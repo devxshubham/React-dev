ab42704ff1d48f45f90437e1ce154c671c2833ee
devxshubham <devshubhamcode@gmail.com>
2024-02-07 23:32:43 +0530

    adding all

---
+++ recoilAdvance/src/atoms.js
@@ -0,0 +1,21 @@
+import {atom, selector} from 'recoil'
+
+
+export const notificationsAtom = atom({
+    key : "notificationsAtom",
+    default : {
+        network : 0,
+        jobs : 0,
+        notification : 0,
+        message : 0
+    }
+})
+
+export const totalNotificationSelector = selector({
+    key : "totalNotificationSelector",
+    get : ({get}) => {
+        const notificaitons = get(notificationsAtom)
+        console.log(notificaitons)
+        return notificaitons.network + notificaitons.jobs + notificaitons.notification + notificaitons.message
+    }
+})
